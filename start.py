45c59ac73b336635dad9d4765e6840fa1b20c666
Arthur Bochkov <arthur.bochkov@gmail.com>
2025-03-28 21:54:12 +0300

    1

---
+++ start.py
@@ -0,0 +1,19 @@
+"""
+1. Из всех методов списка (list) выбрать 5 тех, которые по вашему мнению используются чаще всего
+2. Написать их через запятую с параметрами
+3. Повторить процедуру для словарей (dict), множеств (set), строк (str)
+Задание творческое правильного ответа нет, предлагаю логически подумать какие методы наиболее популярные
+Методы можно посмотреть по ссылкам:
+. https://pythonworld.ru/tipy-dannyx-v-python/spiski-list-funkcii-i-metody-spiskov.html
+. https://pythonworld.ru/tipy-dannyx-v-python/stroki-funkcii-i-metody-strok.html
+. https://pythonworld.ru/tipy-dannyx-v-python/mnozhestva-set-i-frozenset.html
+. https://pythonworld.ru/tipy-dannyx-v-python/slovari-dict-funkcii-i-metody-slovarej.html
+"""
+
+"""
+Ответ:
+Списки: append(element), extend(iterable), remove(value), pop(index), sort(reverse=False)
+Словари: get(key, default=None), update(other_dict), keys(), values(), items()
+Множества: add(element), remove(element), union(other_set), intersection(other_set), difference(other_set)
+Строки: split(separator), join(iterable), replace(old, new), strip(chars), find(substring)
+"""
